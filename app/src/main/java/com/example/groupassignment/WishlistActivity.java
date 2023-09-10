65e0fbbf261e71fc2fa5c380316cc64aaa99f1ee
Windy27 <ziyikhoo456@hotmail.com>
2023-09-10 23:27:24 +0800

    Updated

--- app/src/main/java/com/example/groupassignment/WishlistActivity.java
+++ app/src/main/java/com/example/groupassignment/WishlistActivity.java
@@ -45,6 +45,33 @@ public class WishlistActivity extends AppCompatActivity {
             }
         });
 
+        Button robotBtn = (Button) findViewById(R.id.robotBtn);
+        robotBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WishlistActivity.this, ChatbotActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button communityBtn = (Button) findViewById(R.id.communityBtn);
+        communityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WishlistActivity.this, CommunityActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button profileBtn = (Button) findViewById(R.id.profileBtn);
+        profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WishlistActivity.this, Profile.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
